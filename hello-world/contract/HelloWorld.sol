48d0d10dfd9eec519a0203fe0ddba008d80e1fd8
Remix Workspace <>
2022-03-11 17:08:09 +0100

    created contract

---
+++ hello-world/contract/HelloWorld.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract HelloWorld {
+    event UpdatedMessages(string oldStr, string newStr);
+
+    string public message;
+
+    constructor (string memory initMessage) {
+        message = initMessage;
+    }
+
+    function updateMessage(string memory newMessage) public {
+        string memory oldMsg = message;
+        message = newMessage;
+        emit UpdatedMessages(oldMsg, newMessage);
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
